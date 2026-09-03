2f5ba27058ed460c9275e40fe39e652cc75ccc7c
Umesh Tupdale <tupdaleumesh@gmail.com>
2026-09-03 14:36:06 +0530

    Add icons, fix manifest

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+@'
+const CACHE = "hall14-v2";
+const FILES = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+'@ | Set-Content -Encoding UTF8 public\sw.js
