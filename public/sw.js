079e695e1d5991864b68434332d8a5af33c6e4e0
Umesh Tupdale <tupdaleumesh@gmail.com>
2026-09-03 18:11:34 +0530

    New login screen showing what the app does

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 ﻿@'
-const CACHE = "hall14-v3";
+const CACHE = "hall14-v4";
 const FILES = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
